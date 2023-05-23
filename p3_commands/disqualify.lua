5e65bd4e6af34dd863bef0c1c79a8a9a2df31fe5
Cuh4 <roozzguy@gmail.com>
2023-05-23 07:10:28 +0100

    for the cuh

---
+++ p3_commands/disqualify.lua
@@ -0,0 +1,37 @@
+---------------------------------------
+------------- Command - Disqualify
+---------------------------------------
+
+------------- ?disqualify
+cuhFramework.commands.create("disqualify", {"di"}, false, function(message, peer_id, admin, auth, command, ...)
+    -- Get variables
+    local player = cuhFramework.players.getPlayerByPeerId(peer_id)
+    local args = {...}
+
+    -- Check
+    if miscellaneousLibrary.unnamedClientOrServerOrDisconnecting(player) or not admin then
+        return
+    end
+
+    -- Main
+    if not args[1] then
+        return announceLibrary.status.failure("provide peer id", player)
+    end
+
+    -- Disqualify
+    local target = cuhFramework.players.getPlayerByPeerId(tonumber(args[1]))
+
+    if not target or miscellaneousLibrary.unnamedClientOrServerOrDisconnecting(target) then
+        return announceLibrary.status.failure("invalid", player)
+    end
+
+    if playerStatesLibrary.hasState(target, "disqualify") then
+        -- remove
+        playerStatesLibrary.removeState(target, "disqualify")
+        chatAnnounce(target.properties.name.." is now a participant.")
+    else
+        -- add
+        playerStatesLibrary.setState(target, "disqualify")
+        chatAnnounce(target.properties.name.." has been eliminated.")
+    end
+end, "")
---
+++ p3_commands/say.lua
@@ -0,0 +1,42 @@
+---------------------------------------
+------------- Command - Say
+---------------------------------------
+
+------------- ?say
+cuhFramework.commands.create("say", {"s"}, false, function(message, peer_id, admin, auth, command, ...)
+    -- Get variables
+    local player = cuhFramework.players.getPlayerByPeerId(peer_id)
+    local args = {...}
+
+    -- Check
+    if miscellaneousLibrary.unnamedClientOrServerOrDisconnecting(player) or not admin then
+        return
+    end
+
+    -- Main
+    if not args[1] then
+        return announceLibrary.status.failure("provide type | 1 = cuh says, 0 = no cuh says, just say", player)
+    end
+
+    if args[1] == "1" then
+        -- cuh says
+        table.remove(args, 1)
+        announceLibrary.popupAnnounce("[Cuh Says]\n"..table.concat(args, " "), 6)
+    else
+        -- cuh no say
+        table.remove(args, 1)
+        announceLibrary.popupAnnounce(table.concat(args, " "), 6)
+    end
+
+    -- Effects
+    local vehicle = cuhFramework.vehicles.spawnAddonVehicle(1, cuhFramework.utilities.matrix.offsetPosition((player:get_position()), 0, -10, 0))
+    self = cuhFramework.callbacks.onVehicleLoad:connect(function(vehicle_id)
+        if vehicle_id == vehicle.properties.vehicle_id then
+            self:disconnect()
+            vehicle:press_button("start")
+            cuhFramework.utilities.delay.create(3, function()
+                vehicle:despawn()
+            end)
+        end
+    end)
+end, "")
