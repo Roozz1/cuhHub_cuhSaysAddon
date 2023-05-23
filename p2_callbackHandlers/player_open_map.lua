fc1e0c445bdd400dfe4af353c51323d323740706
Cuh4 <roozzguy@gmail.com>
2023-05-23 07:24:49 +0100

    play area ui

---
+++ p2_callbackHandlers/player_open_map.lua
@@ -0,0 +1,39 @@
+---------------------------------------
+------------- Player Open Map
+---------------------------------------
+
+cuhFramework.callbacks.onToggleMap:connect(function(peer_id, opened)
+    -- Get variables
+    local player = cuhFramework.players.getPlayerByPeerId(peer_id)
+
+    -- Checks
+    if not player then
+        return
+    end
+
+    if miscellaneousLibrary.unnamedClientOrServerOrDisconnecting(player) then
+        return
+    end
+
+    -- Show play area
+    local spawnPoint = globalStorage:get("spawn_point") or matrix.translation(0, 0, 0)
+    local color = colorLibrary.RGB.new(0, 125, 255, 255)
+
+    server.removeMapObject(peer_id, peer_id + 15000)
+    server.addMapObject(
+        peer_id,
+        peer_id + 15000,
+        0,
+        9,
+        spawnPoint[13],
+        spawnPoint[15],
+        0,
+        0,
+        0,
+        0,
+        "Play Area",
+        config.game.playAreaSize,
+        "You cannot leave this area.",
+        colorLibrary.RGB.unpack(color)
+    )
+end)
