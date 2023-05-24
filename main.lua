169c3d1eb90b7e1b156af8dad9dd9d5e2889310e
Cuh4 <roozzguy@gmail.com>
2023-05-24 20:45:50 +0100

    moved ui to main.lua. much cleaner

--- main.lua
+++ main.lua
@@ -107,3 +107,88 @@ cuhFramework.customZones.createPlayerZone(getSpawnPoint(), config.game.playAreaS
     player:teleport(getSpawnPoint())
     chatAnnounce("You cannot leave the play area.", player)
 end)
+
+----------------------------------------------------------------
+-- UI
+----------------------------------------------------------------
+------------- UI
+---@param player player
+eventsLibrary.get("playerJoin"):connect(function(player)
+    -- Participant Status
+    local status = cuhFramework.ui.screen.create(player.properties.peer_id + 15000, "Participant", 0, -0.6, player)
+
+    cuhFramework.utilities.loop.create(0.1, function(id)
+        -- Quick check to see if player still exists
+        if not cuhFramework.players.getPlayerByPeerId(player.properties.peer_id) then
+            cuhFramework.utilities.loop.ongoingLoops[id] = nil
+            return
+        end
+
+        -- Prepare UI Text
+        local to_show = cuhFramework.utilities.miscellaneous.switchbox(
+            "Participant [:)]",
+            "Eliminated [!]",
+            playerStatesLibrary.hasState(player, "disqualify")
+        )
+
+        -- Make sure UI is all good
+        if not status then
+            return
+        end
+
+        -- Andddd, edit the UI
+        status:edit(to_show)
+    end)
+
+    -- Nametag [Physical]
+    local object_id = player:get_character()
+    local nametag = easyPopupsLibrary.physical.create(player.properties.name, matrix.translation(0, 3, 0), object_id, player.properties.peer_id + 17000, nil, 5)
+
+    cuhFramework.utilities.loop.create(0.1, function(id)
+        -- Quick check to see if player still exists
+        if not cuhFramework.players.getPlayerByPeerId(player.properties.peer_id) then
+            cuhFramework.utilities.loop.ongoingLoops[id] = nil
+            return
+        end
+
+        -- Prepare UI text... again
+        local to_show = cuhFramework.utilities.miscellaneous.switchbox(
+            "Participant [:)]",
+            "Eliminated [!]",
+            playerStatesLibrary.hasState(player, "disqualify")
+        )
+
+        if player.properties.admin then
+            to_show = "Enforcer [!!]"
+        end
+
+        -- Edit thy nametag UI
+        nametag:edit(to_show)
+    end)
+end)
+
+------------- Map UI
+---@param player player
+eventsLibrary.get("openMap"):connect(function(player)
+    -- Play Area
+    local spawnPoint = getSpawnPoint()
+    local color = colorLibrary.RGB.new(0, 125, 255, 255)
+
+    server.removeMapObject(player.properties.peer_id, player.properties.peer_id + 15000)
+    server.addMapObject(
+        player.properties.peer_id,
+        player.properties.peer_id + 15000,
+        0,
+        9,
+        spawnPoint[13],
+        spawnPoint[15],
+        0,
+        0,
+        0,
+        0,
+        "Play Area",
+        config.game.playAreaSize,
+        "You cannot leave this area.",
+        colorLibrary.RGB.unpack(color)
+    )
+end)
